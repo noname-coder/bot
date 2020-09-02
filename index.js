2cd28c151c335f54cc33dfdf77956b493f8337d8
noname-coder <70646438+noname-coder@users.noreply.github.com>
2020-09-02 19:26:17 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,69 @@
-const Telegraf = require('telegraf')
+const TelegramBot = require('node-telegram-bot-api'); // подключаем node-telegram-bot-api
 
-const bot = new Telegraf(process.env.1308047539:AAHAmthHFIU0_96x-ZxftLmK8J5NkA00sMA)
-bot.start((ctx) => ctx.reply('Welcome!'))
-bot.help((ctx) => ctx.reply('Send me a sticker'))
-bot.on('sticker', (ctx) => ctx.reply(''))
-bot.hears('hi', (ctx) => ctx.reply('Hey there'))
-bot.launch()
+const token = '1308047539:AAHAmthHFIU0_96x-ZxftLmK8J5NkA00sMA'; // тут токен кторый мы получили от botFather
+
+// включаем самого обота
+const bot = new TelegramBot(token, {polling: true});
+
+//конфиг клавиатуры
+const keyboard = [
+    [
+      {
+        text: 'Хочу кота', // текст на кнопке
+        callback_data: 'moreKeks' // данные для обработчика событий
+      }
+    ],
+    [
+        {
+          text: 'Хочу песика',
+          callback_data: 'morePes'
+        }
+    ],
+    [
+        {
+          text: 'Хочу проходить курсы',
+          url: 'https://htmlacademy.ru/continue' //внешняя ссылка
+        }
+      ]
+  ];
+
+// обработчик события присылания нам любого сообщения
+bot.on('message', (msg) => {
+  const chatId = msg.chat.id; //получаем идентификатор диалога, чтобы отвечать именно тому пользователю, который нам что-то прислал
+
+  // отправляем сообщение
+  bot.sendMessage(chatId, 'Привет, Друг! чего хочешь?', { // прикрутим клаву
+        reply_markup: {
+            inline_keyboard: keyboard
+        }
+    });
+});
+
+// обработчик событий нажатий на клавиатуру
+bot.on('callback_query', (query) => {
+    const chatId = query.message.chat.id;
+
+    let img = '';
+
+    if (query.data === 'moreKeks') { // если кот
+        img = 'keks.png';
+    }
+
+    if (query.data === 'morePes') { // если пёс
+        img = 'pes.png';
+    }
+
+    if (img) {
+        bot.sendPhoto(chatId, img, { // прикрутим клаву
+            reply_markup: {
+                inline_keyboard: keyboard
+            }
+        });
+    } else {
+        bot.sendMessage(chatId, 'Непонятно, давай попробуем ещё раз?', { // прикрутим клаву
+            reply_markup: {
+                inline_keyboard: keyboard
+            }
+        });
+    }
+  });
